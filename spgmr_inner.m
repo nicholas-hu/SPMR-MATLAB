03713f5dfbd98b6c87e11f067c1386cae134c843
Nicholas Hu <nicholas.hu@alumni.ubc.ca>
2019-04-26 01:15:54 -0700

    Scaling of tolerance in inner SPMR

--- spgmr_inner.m
+++ spgmr_inner.m
@@ -1,8 +1,11 @@
 function sol = spgmr_inner(K, rhs, maxit)
-    x_hat = ldiv(K.A, rhs(1:K.n));
+    x_hat   = ldiv(K.A, rhs(1:K.n));
+    g       = rhs(K.n + (1:K.m)) - K.G2 * x_hat;
     
-    result = spmr_sc(K, rhs(K.n + (1:K.m)) - K.G2 * x_hat, ...
-                     'tol', 1e-10, 'maxit', maxit);
+    % The tolerance must be scaled as it is *relative*
+    tol     = norm(rhs) / norm(g) * 1e-10;
     
-    sol = [result.x + x_hat; result.y];
+    result  = spmr_sc(K, g, 'tol', tol, 'maxit', maxit);
+    
+    sol     = [result.x + x_hat; result.y];
 end
